73590cb0761627eeaffc85df5ed18cd79d0aaddc
isabellacopland <isabella.copland@gmail.com>
2022-04-06 21:33:48 +0100

    getting older added

--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,11 @@
 function Pet() {}
 module.exports = Pet;
 
-function Pet(name) {
+  function Pet(name) {
     this.name = name;
+    this.age = 0;
   }
+
+  Pet.prototype.growUp = function() {
+    this.age += 1;
+  };
