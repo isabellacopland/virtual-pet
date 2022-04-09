28288e26ff69bf30904766fbb33a1a3f54174d8b
isabellacopland <isabella.copland@gmail.com>
2022-04-09 14:50:42 +0100

    ageing, fitness and walking added to pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,28 +4,42 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-  });
   
     it('sets the name property', () => {
       const pet = new Pet('Fido');
   
       expect(pet.name).toEqual('Fido');
     });
+
+    it('has a initial age of 0, hunger of 0 and fitness of 10', () => {
+      const pet = new Pet('Fido');
+
+     expect(pet.age).toEqual(0);
+     expect(pet.hunger).toEqual(0);
+     expect(pet.fitness).toEqual(10);
+    });
   });
 
-  it('has a initial age of 0', () => {
-    const pet = new Pet('Fido');
-
-    expect(pet.age).toEqual(0);
-  });
-});
 
 describe('growUp', () => {
-  it('increments the age by 1', () => {
+  it('increments the age by 1, hunger by 5 and fitness by -3', () => {
     const pet = new Pet('Fido');
 
     pet.growUp();
 
     expect(pet.age).toEqual(1);
+    expect(pet.hunger).toEqual(5);
+    expect(pet.fitness).toEqual(7);
+  });
+});
+
+describe ('walk', () => {
+  it('increases fitness by 4 to a maximum of 10', () => {
+    const pet = new Pet('Fido');
+
+    pet.fitness = 8;
+    pet.walk();
+
+    expect(pet.fitness).toEqual(10);
   });
 });
